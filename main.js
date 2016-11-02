ee5e1b7744635ff8395c8da50da79445f91633d8
MaciejLitwiniec <MaciejLitwiniec@users.noreply.github.com>
2016-11-02 23:06:56 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+
+function loadDocPL() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
+   }
+ };
+ xhttp.open("GET", "contentPL.txt", true);
+ xhttp.send();
+}
+ 
+function loadDocEN() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
+   }
+ };
+ xhttp.open("GET", "contentEN.txt", true);
+ xhttp.send();
+} 
